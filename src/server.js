ee1f5fb75b4e2132941298eac4d76c8dc25e955f
arnWolff <arnwolff@gmail.com>
2022-02-25 16:06:19 +0100

    commit main#1

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const db = require("./db/DBConnection");
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
-  console.log("Express server started and running!");
+  console.log("Express server started and running!" ,port);
 });
